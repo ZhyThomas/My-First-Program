fa4841585fb8889efb8bfed38f264d93d8f3a1bf
ZhyGitHub <15858181756@163.com>
2017-07-01 23:45:41 +0800

    优化

--- js/common.js
+++ js/common.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-    document.querySelector('#form1').addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert("成功阻止submit默认事件");
-    }, false);
-        var x = ($('html').height() - 700) / 2;
-        $('body').css("margin-top", x);
-    $(window).resize(function() {
-    	var x = ($('html').height() - 700) / 2;             //保持body垂直居中。
-        $('body').css("margin-top", x);
-    });
+    
+    // var x = ($('html').height() - 700) / 2;
+    // $('body').css("margin-top", x);
+    // $(window).resize(function() {
+    //     var x = ($('html').height() - 700) / 2; //保持body垂直居中。
+    //     $('body').css("margin-top", x);
+    // });
 
 })
--- js/login.js
+++ js/login.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
+    $("form").submit(function(e) {
+        e.preventDefault();        //阻止表单默认提交
+        $.ajax({
+            url: "http://bimqh.ecidi.com:6530/accounts/api/user/login2/",
+            type: 'POST',
+            timeout: 10000,
+            //contentType:"application/json",错误
+            data: $("form").serialize(),         
+            success: function(d) {
+                alert(d.account.person_name + "您好"); //注意:d是对象
+            },
+            error: function(jqxhr, textStatus, errorThrow) { alert(errorThrow); },
+        });
+    });
     $("#logbtn").click(function() {
         var username = $("#username").val();
         var password = $("#password").val();
         $.ajax({
             url: "http://bimqh.ecidi.com:6530/accounts/api/user/login2/",
-            type: 'post',
-            dataType: 'json',
+            type: 'POST',
             timeout: 10000,
+            //contentType:"application/json",错误
             data: {
-                username: username,
-                password: password
+                "username": username,
+                "password": password
             },
             success: function(d) {
-                alert(d.account.person_name + "您好"); //注意:json是对象
+                alert(d.account.person_name + "您好"); //注意:d是对象
             },
             error: function(jqxhr, textStatus, errorThrow) { alert(errorThrow); },
-            statusCode: {
-                404: function() { alert('not found'); },
-                500: function() { alert('error by server'); },
-            }
         });
     });
 
